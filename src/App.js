c31d0db1a802d0131dda377b46791b81b60f50c2
Xiang Gao <gx7181@gmail.com>
2021-08-03 16:39:44 -0700

    create homepage and basic layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import HomePage from "./Components/homepage/homePage.jsx";
+import HomePage from "./Pages/homepage/homePage.jsx";
 function App() {
   return (
     <div className="App">
