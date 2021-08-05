a630997c40e8ae64b734d5289b96dd2b154b1cf0
Xiang Gao <gx7181@gmail.com>
2021-08-04 18:34:33 -0700

    minor update

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./Pages/homepage/homePage.jsx";
 import ShopPage from "./Pages/shopPage/shopPage.jsx";
-
+import Header from "./Components/header/header";
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
