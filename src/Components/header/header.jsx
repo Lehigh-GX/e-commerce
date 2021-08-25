66bfbe9fae0605877e152f6b0e1685d23af36579
Xiang Gao <gx7181@gmail.com>
2021-08-24 17:46:42 -0700

    implement redux in cart

--- src/Components/header/header.jsx
+++ src/Components/header/header.jsx
@@ -4,7 +4,10 @@ import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firesbase.utils";
 import { connect } from "react-redux";
 import "./header.scss";
-const Header = ({ currentUser }) => (
+import CartIcon from "../cart-icon/cart-icon";
+import CartDropdown from "../cart-dropDown/cart-dropDown";
+
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -25,10 +28,13 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
 );
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 export default connect(mapStateToProps)(Header);
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,7 +1,14 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import logger from "redux-logger";
 import rootReducer from "../root-reducer";
 const middleWare = [logger];
-const store = createStore(rootReducer, applyMiddleware(...middleWare));
+const store = createStore(
+  rootReducer,
+  compose(
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(...middleWare)
+  )
+);
 
 export default store;
